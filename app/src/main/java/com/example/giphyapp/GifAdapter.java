5dffe439fc36ca031c02c7957ea76b222a16543d
ShwetaKhanal <200556272@student.georgianc.on.ca>
2024-11-30 19:34:24 -0500

    added the download functionaly to the app

--- app/src/main/java/com/example/giphyapp/GifAdapter.java
+++ app/src/main/java/com/example/giphyapp/GifAdapter.java
@@ -1,9 +1,14 @@
 package com.example.giphyapp;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,9 +37,32 @@ public class GifAdapter extends RecyclerView.Adapter<GifAdapter.GifViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull GifViewHolder holder, int position) {
         GiphyResponse.Data gif = gifs.get(position);
+
+        // Load GIF into the ImageView using Glide
         Glide.with(holder.itemView.getContext())
                 .load(gif.getImages().getOriginal().getUrl())
                 .into(holder.imageView);
+
+        // Handle download button click
+        holder.downloadButton.setOnClickListener(v -> {
+            Context context = holder.itemView.getContext();
+            String gifUrl = gif.getImages().getOriginal().getUrl();
+            String fileName = gifUrl.substring(gifUrl.lastIndexOf('/') + 1);
+
+            // Use DownloadManager to download the GIF
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            if (downloadManager != null) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(gifUrl));
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                // Enqueue the download request
+                downloadManager.enqueue(request);
+                Toast.makeText(context, "Downloading " + fileName, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "DownloadManager not available", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,10 +72,13 @@ public class GifAdapter extends RecyclerView.Adapter<GifAdapter.GifViewHolder> {
 
     public static class GifViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        ImageView downloadButton;
 
         public GifViewHolder(@NonNull View itemView) {
             super(itemView);
+
             imageView = itemView.findViewById(R.id.gifImage);
+            downloadButton = itemView.findViewById(R.id.btnDownload);
         }
     }
 }
